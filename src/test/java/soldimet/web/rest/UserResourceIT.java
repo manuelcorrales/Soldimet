600ceaaf45b33e4ad0892ecf8d98e560f922e927
manuelc <manuelc@eventbrite.com>
2019-09-27 17:15:52 -0300

    Changed to docker

--- src/test/java/soldimet/web/rest/UserResourceIT.java
+++ src/test/java/soldimet/web/rest/UserResourceIT.java
@@ -498,7 +498,7 @@ public class UserResourceIT {
             .andExpect(status().isOk())
             .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
             .andExpect(jsonPath("$").isArray())
-            .andExpect(jsonPath("$").value(hasItems(AuthoritiesConstants.USER, AuthoritiesConstants.ADMIN)));
+            .andExpect(jsonPath("$").value(hasItems(AuthoritiesConstants.ADMIN)));
     }
 
     @Test
