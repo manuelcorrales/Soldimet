8ca80c16247edf686e199752219d395c5ae987e9
manuelc <manuelc@eventbrite.com>
2021-04-05 19:21:12 -0300

    Fix private method

--- src/main/webapp/app/reports/reportes.component.ts
+++ src/main/webapp/app/reports/reportes.component.ts
@@ -140,7 +140,7 @@ export class ReportesComponent implements OnInit {
     );
   }
 
-  private limpiarFiltros() {
+  limpiarFiltros() {
     this.limpiarFiltrado = false;
     this.fechaDesde = null;
     this.formGroup.get('FechaDesde').setValue('');
